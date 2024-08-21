5ff52d347c5a88286b7d8bbfe97edd8a7565d11e
zxcv <edward.kang0301@gmail.com>
2024-08-21 17:31:37 +1000

    Added bonus, new project to compress files to a folder

---
+++ Bonus/bonus11.py
@@ -0,0 +1,25 @@
+# File Zipper titled Window
+# Select files to compress: # Choose Button, opens up file explorer, multi-select
+# Select destination folder: # Choose Button, open up a directory
+# Compress Button
+
+import FreeSimpleGUI as gui
+
+label1 = gui.Text("Select Files to compress: ")
+label2 = gui.Text("Select Destination Folder: ")
+input_box1 = gui.Input()
+input_box2 = gui.Input()
+choose_files = gui.FilesBrowse("Choose")
+choose_location = gui.FolderBrowse("Choose")
+compress = gui.Button("Compress")
+compress_button = gui.Button("Compression was completed!")
+
+window = gui.Window('File Zipper',
+                    layout=[
+                        [label1, input_box1, choose_files],
+                        [label2, input_box2, choose_location],
+                        [compress]
+                    ])
+
+window.read()
+window.close()
