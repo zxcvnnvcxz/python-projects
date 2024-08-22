0c98d1f4f453ea8e3a4a179d35788b82797d74ac
zxcv <edward.kang0301@gmail.com>
2024-08-22 17:47:59 +1000

    Implemented fucntional edit button

--- gui.py
+++ gui.py
@@ -2,24 +2,41 @@ import functions
 import FreeSimpleGUI as gui
 
 label = gui.Text("Type in a to-do")
-input_box = gui.InputText(tooltip="Enter todo",
-                          key="todo")
+input_box = gui.InputText(tooltip="Enter todo", key="todo")
 add_button = gui.Button("Add")
+list_box = gui.Listbox(values =functions.get_todos(), key='todos',
+                       enable_events=True, size=[45, 10])
+edit_button = gui.Button("Edit")
 
 window = gui.Window('My To-Do App',
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button]],
                     font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case gui.WIN_CLOSED:
             break
 
