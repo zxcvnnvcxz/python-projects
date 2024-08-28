0415f154ccb64c92ce692809b6451a46fcaac8cf
zxcv <edward.kang0301@gmail.com>
2024-08-28 14:17:04 +1000

    Added an exit button, complete

--- gui.py
+++ gui.py
@@ -4,14 +4,17 @@ import FreeSimpleGUI as gui
 label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter todo", key="todo")
 add_button = gui.Button("Add")
-list_box = gui.Listbox(values =functions.get_todos(), key='todos',
+list_box = gui.Listbox(values=functions.get_todos(), key='todos',
                        enable_events=True, size=[45, 10])
 edit_button = gui.Button("Edit")
+complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
 window = gui.Window('My To-Do App',
                     layout=[[label],
                             [input_box, add_button],
-                            [list_box, edit_button]],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -26,6 +29,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -35,9 +39,20 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'todos':
             window['todo'].update(value=values['todos'][0])
+
         case gui.WIN_CLOSED:
+            break
+        case "Exit":
             break
 
 
